dc0c34993cc57d554bdda41318bf3cc5cc45f3bb
Jihan Ali Ahmad <jihanaliahmad@gmail.com>
2022-01-05 04:21:24 +0700

    efe

--- application/views/wrh/aluminium/v_aluminium_list.php
+++ application/views/wrh/aluminium/v_aluminium_list.php
@@ -105,13 +105,16 @@
                             $i = 1;
                             foreach ($aluminium->result() as $row) {
                                 $ada                     = 1;
-                                $stock_awal_bulan        = @$s_awal_bulan[$row->id];
+                                $stock_awal_bulan        = 0;
+                                // $stock_awal_bulan        = @$s_awal_bulan[$row->id];
                                 $tampil_stock_awal_bulan = ($stock_awal_bulan != '') ? $stock_awal_bulan : 0;
 
-                                $tot_in_per_bulan          = @$total_in_per_bulan[$row->id];
+                                $tot_in_per_bulan          = 0;
+                                // $tot_in_per_bulan          = @$total_in_per_bulan[$row->id];
                                 $tampil_total_in_per_bulan = ($tot_in_per_bulan != '') ? $tot_in_per_bulan : 0;
 
-                                $tot_out_per_bulan          = @$total_out_per_bulan[$row->id];
+                                $tot_out_per_bulan          = 0;
+                                // $tot_out_per_bulan          = @$total_out_per_bulan[$row->id];
                                 $tampil_total_out_per_bulan = ($tot_out_per_bulan != '') ? $tot_out_per_bulan : 0;
 
                                 $stock_akhir_bulan = ($tampil_stock_awal_bulan + $tampil_total_in_per_bulan) - $tampil_total_out_per_bulan;
