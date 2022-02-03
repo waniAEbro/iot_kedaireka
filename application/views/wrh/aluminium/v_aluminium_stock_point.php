d286b852f0a4bb820df0a1ae59a990d367d0929b
Jihan Ali Ahmad <jihanaliahmad@gmail.com>
2022-02-03 15:08:28 +0700

    dsad

--- application/views/wrh/aluminium/v_aluminium_stock_point.php
+++ application/views/wrh/aluminium/v_aluminium_stock_point.php
@@ -3,7 +3,7 @@
     <div class="col-lg-12">
         <div class="box box-primary">
             <div class="box-header with-border">
-                <h3 class="box-title">Stok Point Aksesoris</h3>
+                <h3 class="box-title">Stok Point Aluminium</h3>
             </div>
             <div class="box-body">
                 <div class="row">
