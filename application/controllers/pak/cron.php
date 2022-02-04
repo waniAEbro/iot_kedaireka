2e26cc8a78dc554840b4a8b0f1127e1acb7805bb
Jihan Ali Ahmad <jihanaliahmad@gmail.com>
2022-02-04 09:42:49 +0700

    cron

---
+++ application/controllers/pak/cron.php
@@ -0,0 +1,20 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+class Cron extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('wrh/m_aluminium');
+        $this->load->model('wrh/m_aksesoris');
+    }
+
+    public function cron_stock_point()
+    {
+        $this->db->where('qty <', 1);
+        $this->db->delete('data_counter');
+        $this->m_aksesoris->cekAdaStockPoint(1);
+        $this->m_aksesoris->cekAdaStockPoint(2);
+    }
+}
