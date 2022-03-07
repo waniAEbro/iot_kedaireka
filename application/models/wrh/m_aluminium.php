aa90fcb78266d818844ef831574d64a7d27c57ba
Jihan Ali Ahmad <jihanaliahmad@gmail.com>
2022-03-07 10:51:33 +0700

    cds

--- application/models/wrh/m_aluminium.php
+++ application/models/wrh/m_aluminium.php
@@ -637,6 +637,7 @@ class M_aluminium extends CI_Model
         $this->db->join('master_item mi', 'mi.id = ds.id_item', 'left');
         $this->db->join('master_warna mwa', 'mwa.kode = mi.kode_warna', 'left');
         $this->db->join('master_gudang mg', 'mg.id = ds.id_gudang', 'left');
+        $this->db->join('master_brand mb', 'mb.id = ds.id_multi_brand', 'left');
 
         $this->db->where('ds.id_fppp', $id_fppp);
         $this->db->where('ds.id_jenis_item', $id_jenis_item);
@@ -644,7 +645,7 @@ class M_aluminium extends CI_Model
         // $this->db->where('ds.is_bom', 1);
         // $this->db->where('ds.id_surat_jalan', 0);
         $this->db->where('ds.ke_mf', 1);
-        $this->db->select('ds.*,ds.id as id_stock,mi.*,mwa.warna,mi.divisi,mg.gudang,ds.created as created_,ds.updated as updated_');
+        $this->db->select('ds.*,ds.id as id_stock,mi.*,mwa.warna,mi.divisi,mg.gudang,ds.created as created_,ds.updated as updated_,mb.brand');
 
         $this->db->order_by('ds.id', 'asc');
 
