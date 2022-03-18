51f2673abd757d9bfc1470a45e6cd1fbfb31a44c
Jihan Ali Ahmad <jihanaliahmad@gmail.com>
2022-03-18 13:33:11 +0700

    cdsa

--- application/models/inout/m_aksesoris.php
+++ application/models/inout/m_aksesoris.php
@@ -9,12 +9,13 @@ class M_aksesoris extends CI_Model
         $this->db->join('master_item mi', 'mi.id = ds.id_item', 'left');
         $this->db->join('master_divisi_stock mds', 'mds.id = ds.id_divisi', 'left');
         $this->db->join('master_gudang mg', 'mg.id = ds.id_gudang', 'left');
+        $this->db->join('master_supplier ms', 'ms.id = ds.id_supplier', 'left');
         $this->db->where('ds.id_jenis_item', 2);
         $this->db->where('DATE(ds.aktual) >=', $tgl_awal);
         $this->db->where('DATE(ds.aktual) <=', $tgl_akhir);
         $this->db->where('ds.inout', 1);
         $this->db->where('ds.awal_bulan', 0);
-        $this->db->select('ds.*,mi.*,ds.created as tgl_stok,mds.divisi as divisi_aksesoris,mg.gudang');
+        $this->db->select('ds.*,mi.*,ds.created as tgl_stok,mds.divisi as divisi_aksesoris,mg.gudang,ms.supplier');
         $this->db->order_by('ds.id', 'desc');
 
         return $this->db->get('data_stock ds');
