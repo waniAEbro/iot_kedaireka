00d21ff1780c43d440a564ca451b9eeaebc24983
Jihan Ali Ahmad <jihanaliahmad@gmail.com>
2022-03-21 11:42:22 +0700

    dsd

--- application/models/inout/m_aksesoris.php
+++ application/models/inout/m_aksesoris.php
@@ -11,8 +11,10 @@ class M_aksesoris extends CI_Model
         $this->db->join('master_gudang mg', 'mg.id = ds.id_gudang', 'left');
         $this->db->join('master_supplier ms', 'ms.id = ds.id_supplier', 'left');
         $this->db->where('ds.id_jenis_item', 2);
-        $this->db->where('DATE(ds.aktual) >=', $tgl_awal);
-        $this->db->where('DATE(ds.aktual) <=', $tgl_akhir);
+        // $this->db->where('DATE(ds.aktual) >=', $tgl_awal);
+        // $this->db->where('DATE(ds.aktual) <=', $tgl_akhir);
+        $this->db->where('DATE(ds.created) >=', $tgl_awal);
+        $this->db->where('DATE(ds.created) <=', $tgl_akhir);
         $this->db->where('ds.inout', 1);
         $this->db->where('ds.awal_bulan', 0);
         $this->db->select('ds.*,mi.*,ds.created as tgl_stok,mds.divisi as divisi_aksesoris,mg.gudang,ms.supplier');
@@ -32,8 +34,10 @@ class M_aksesoris extends CI_Model
         
 
         $this->db->where('ds.id_jenis_item', 2);
-        $this->db->where('DATE(dsj.tgl_aktual) >=', $tgl_awal);
-        $this->db->where('DATE(dsj.tgl_aktual) <=', $tgl_akhir);
+        // $this->db->where('DATE(dsj.tgl_aktual) >=', $tgl_awal);
+        // $this->db->where('DATE(dsj.tgl_aktual) <=', $tgl_akhir);
+        $this->db->where('DATE(ds.created) >=', $tgl_awal);
+        $this->db->where('DATE(ds.created) <=', $tgl_akhir);
         $this->db->where('ds.inout', 2);
         $this->db->where('ds.in_temp', 0);
         $this->db->where('ds.id_surat_jalan !=', 0);
