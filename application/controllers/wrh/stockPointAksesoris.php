d465177cb3c2a3d3ed25771fb8958788c6b99386
Jihan Ali Ahmad <jihanaliahmad@gmail.com>
2022-04-04 15:31:03 +0700

    vjefvb

--- application/controllers/wrh/stockPointAksesoris.php
+++ application/controllers/wrh/stockPointAksesoris.php
@@ -18,30 +18,30 @@ class StockPointAksesoris extends CI_Controller
 
     public function save()
     {
-        $tgl = date('Y-m-d');
+        // $tgl = date('Y-m-d');
 
-        $aksesoris = $this->m_aksesoris->getAksesoris();
-        $in = $this->m_aksesoris->getAksesorisStokIn();
-        $out = $this->m_aksesoris->getAksesorisStokOut();
-        $cek = $this->m_aksesoris->cekStockPoint($tgl);
-        if ($cek == 1) {
-            foreach ($aksesoris->result() as $key) {
-                $stock_in = @$in[$key->item_code][$key->id_divisi][$key->id_gudang];
-                $stock_out = @$out[$key->item_code][$key->id_divisi][$key->id_gudang];
-                $real_stock = $stock_in - $stock_out;
-                $data = array(
-                    'tgl' => $tgl,
-                    'item_code' => $key->item_code,
-                    'id_divisi' => $key->id_divisi,
-                    'id_gudang' => $key->id_gudang,
-                    'qty' => $real_stock,
-                );
-                $this->m_aksesoris->saveStockPointAksesoris($data);
-            }
-        }
+        // $aksesoris = $this->m_aksesoris->getAksesoris();
+        // $in = $this->m_aksesoris->getAksesorisStokIn();
+        // $out = $this->m_aksesoris->getAksesorisStokOut();
+        // $cek = $this->m_aksesoris->cekStockPoint($tgl);
+        // if ($cek == 1) {
+        //     foreach ($aksesoris->result() as $key) {
+        //         $stock_in = @$in[$key->item_code][$key->id_divisi][$key->id_gudang];
+        //         $stock_out = @$out[$key->item_code][$key->id_divisi][$key->id_gudang];
+        //         $real_stock = $stock_in - $stock_out;
+        //         $data = array(
+        //             'tgl' => $tgl,
+        //             'item_code' => $key->item_code,
+        //             'id_divisi' => $key->id_divisi,
+        //             'id_gudang' => $key->id_gudang,
+        //             'qty' => $real_stock,
+        //         );
+        //         $this->m_aksesoris->saveStockPointAksesoris($data);
+        //     }
+        // }
 
-        $data['tgl'] = $tgl;
-        $data['stock_point'] = $this->m_aksesoris->getStockPoint($tgl);
+        // $data['tgl'] = $tgl;
+        // $data['stock_point'] = $this->m_aksesoris->getStockPoint($tgl);
     }
 }
 
