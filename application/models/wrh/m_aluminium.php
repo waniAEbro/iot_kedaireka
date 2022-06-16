434ddfbd9338966dec2ab4132a78e24a272f342a
Jihan Ali Ahmad <jihanaliahmad@gmail.com>
2022-06-16 14:10:16 +0700

    cek

--- application/models/wrh/m_aluminium.php
+++ application/models/wrh/m_aluminium.php
@@ -7,22 +7,33 @@ class M_aluminium extends CI_Model
     public function getdata()
     {
         $id_jenis_item = 1;
+        $this->db->join('master_item mi', 'mi.id = dc.id_item', 'left');
         $this->db->join('master_warna mwa', 'mwa.kode = mi.kode_warna', 'left');
         $this->db->where('mi.id_jenis_item', $id_jenis_item);
         $this->db->where('mi.kode_warna !=', '01');
+        $this->db->where('dc.qty >', 0);
         $this->db->select('mi.*,mwa.warna');
+        $this->db->group_by('mi.id');
         // $this->db->limit(100);
-        return $this->db->get('master_item mi');
+        // return $this->db->get('master_item mi');
+        
+        return $this->db->get('data_counter dc');
+        
     }
 
     public function getdataMf()
     {
         $id_jenis_item = 1;
+        $this->db->join('master_item mi', 'mi.id = dc.id_item', 'left');
         $this->db->join('master_warna mwa', 'mwa.kode = mi.kode_warna', 'left');
         $this->db->where('mi.id_jenis_item', $id_jenis_item);
         $this->db->where('mi.kode_warna', '01');
+        $this->db->where('dc.qty >', 0);
+        
         $this->db->select('mi.*,mwa.warna');
-        return $this->db->get('master_item mi');
+        $this->db->group_by('mi.id');
+        // return $this->db->get('master_item mi');
+        return $this->db->get('data_counter dc');
     }
 
     public function getdatapaging($num = false, $keyword = '', $perpage = '', $offset = '')
