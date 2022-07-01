433232f8f5e2c5382ab39c6424bdbbe36546a900
Jihan Ali Ahmad <jihanaliahmad@gmail.com>
2022-07-01 22:01:58 +0700

    inout hrb

---
+++ application/controllers/inout_h/aluminium.php
@@ -0,0 +1,74 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class aluminium extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->fungsi->restrict();
+        $this->load->model('inout_h/m_aluminium');
+    }
+
+    public function index()
+    {
+        $this->fungsi->check_previleges('inout');
+        $bulan       = date('m');
+        $tahun       = date('Y');
+        $data['tgl_awal']  = $tahun . '-' . $bulan . '-01';
+        $data['tgl_akhir'] = date("Y-m-t", strtotime($data['tgl_awal']));
+        $data['aluminium']    = $this->m_aluminium->getData($data['tgl_awal'], $data['tgl_akhir']);
+
+        $this->load->view('inout_h/aluminium/v_aluminium_list', $data);
+    }
+
+    public function diSet($tgl_awal = '', $tgl_akhir = '')
+    {
+        $this->fungsi->check_previleges('inout');
+        $data['tgl_awal']         = $tgl_awal;
+        $data['tgl_akhir']        = $tgl_akhir;
+        $data['aluminium']    = $this->m_aluminium->getData($data['tgl_awal'], $data['tgl_akhir']);
+        $this->load->view('inout_h/aluminium/v_aluminium_list', $data);
+    }
+
+    public function diSetCetak($tgl_awal = '', $tgl_akhir = '')
+    {
+        $data['tgl_awal']         = $tgl_awal;
+        $data['tgl_akhir']        = $tgl_akhir;
+        $data['aluminium']    = $this->m_aluminium->getData($data['tgl_awal'], $data['tgl_akhir']);
+        $this->load->view('inout_h/aluminium/v_aluminium_list_cetak', $data);
+    }
+
+    public function out()
+    {
+        $this->fungsi->check_previleges('inout');
+        $bulan       = date('m');
+        $tahun       = date('Y');
+        $data['tgl_awal']  = $tahun . '-' . $bulan . '-01';
+        $data['tgl_akhir'] = date("Y-m-t", strtotime($data['tgl_awal']));
+        $data['aluminium']    = $this->m_aluminium->getDataOut($data['tgl_awal'], $data['tgl_akhir']);
+
+        $this->load->view('inout_h/aluminium/v_aluminium_list_out', $data);
+    }
+
+    public function diSetOut($tgl_awal = '', $tgl_akhir = '')
+    {
+        $this->fungsi->check_previleges('inout');
+        $data['tgl_awal']         = $tgl_awal;
+        $data['tgl_akhir']        = $tgl_akhir;
+        $data['aluminium']    = $this->m_aluminium->getDataOut($data['tgl_awal'], $data['tgl_akhir']);
+        $this->load->view('inout_h/aluminium/v_aluminium_list_out', $data);
+    }
+
+    public function diSetCetakOut($tgl_awal = '', $tgl_akhir = '')
+    {
+        $data['tgl_awal']         = $tgl_awal;
+        $data['tgl_akhir']        = $tgl_akhir;
+        $data['aluminium']    = $this->m_aluminium->getDataOut($data['tgl_awal'], $data['tgl_akhir']);
+        $this->load->view('inout_h/aluminium/v_aluminium_list_cetak_out', $data);
+    }
+}
+
+/* End of file aluminium.php */
+/* Location: ./application/controllers/inout_h/aluminium.php */
---
+++ application/views/inout_h/aluminium/v_aluminium_list.php
@@ -0,0 +1,142 @@
+<div class="row">
+    <div class="col-lg-12">
+        <div class="box box-primary">
+            <div class="box-header with-border">
+                <h3 class="box-title">Rekap In Aluminium</h3>
+            </div>
+            <div class="box-body">
+                <?php
+                $level = from_session('level');
+                if ($level != '4') {
+
+                ?>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>Tanggal Awal</label>
+                                <input type="text" data-date-format="yyyy-mm-dd" value="<?= $tgl_awal ?>" class="form-control datepicker" autocomplete="off" id="tgl_awal">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>Tanggal Akhir</label>
+                                <input type="text" data-date-format="yyyy-mm-dd" value="<?= $tgl_akhir ?>" class="form-control datepicker" autocomplete="off" id="tgl_akhir">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="box-tools pull-right">
+                                <a class="btn btn-primary" onclick="cetakExcel()">Cetak</a>
+                                <a class="btn btn-success" onclick="setAplikator()">Set Filter</a>
+                            </div>
+                        </div>
+                        </>
+                    <?php } ?>
+                    </div>
+                    <div class="box-body">
+                        <table width="100%" id="tableku" class="table table-striped">
+                            <thead>
+                                <th width="5%">No</th>
+                                <th>Tgl Aktual</th>
+                                <th>Tgl</th>
+                                <th>User</th>
+                                <th>Stok Awal Bulan</th>
+                                <th>Item Code</th>
+                                <th width="25%">Deskripsi Warna</th>
+                                <th>Satuan</th>
+                                <th>Qty</th>
+                                <th>Divisi</th>
+                                <th>Gudang</th>
+                                <th>Keranjang</th>
+                                <th>Supplier</th>
+                                <th>NO Surat Jalan</th>
+                                <th>NO PR/WO</th>
+                                <th>Keterangan</th>
+                                <th>Status</th>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $i = 1;
+                                foreach ($aluminium->result() as $row) :
+                                    $sts = ($row->mutasi == 0) ? "Stock" : "Mutasi";
+                                    $qty = ($row->inout == 1) ? $row->qty_in : $row->qty_out;
+                                    $awal_bulan = ($row->awal_bulan == 1) ? 'YA' : 'TIDAK';
+                                ?>
+                                    <tr>
+                                        <td align="center"><?= $i++ ?></td>
+                                        <td><?= $row->aktual ?></td>
+                                        <td><?= $row->tgl_stok ?></td>
+                                        <td><?= $row->nama ?></td>
+                                        <td><?= $awal_bulan ?></td>
+                                        <td><?= $row->section_ata . '-' . $row->section_allure . '-' . $row->temper . '-' . $row->kode_warna . '-' . $row->ukuran ?></td>
+
+                                        <td><?= $row->warna ?></td>
+                                        <td><?= $row->satuan ?></td>
+                                        <td><?= $qty ?></td>
+                                        <td><?= $row->divisi ?></td>
+                                        <td><?= $row->gudang ?></td>
+                                        <td><?= $row->keranjang ?></td>
+                                        <td><?= $row->supplier ?></td>
+                                        <td><?= $row->no_surat_jalan ?></td>
+                                        <td><?= $row->no_pr ?></td>
+                                        <td><?= $row->keterangan ?></td>
+                                        <td><?= $sts ?></td>
+
+                                    </tr>
+
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script language="javascript">
+    $(document).ready(function() {
+        $('select').select2();
+        $('.datepicker').datepicker({
+            autoclose: true
+        });
+        var table = $('#tableku').DataTable({
+            "ordering": true,
+            "scrollX": true,
+        });
+    });
+
+    function setAplikator(argument) {
+        var tgl_awal = $('#tgl_awal').val();
+        if (tgl_awal != '') {
+            var tlg1 = tgl_awal;
+        } else {
+            var tlg1 = '<?= $tgl_awal ?>';
+        };
+        var tgl_akhir = $('#tgl_akhir').val();
+        if (tgl_akhir != '') {
+            var tgl2 = tgl_akhir;
+        } else {
+            var tgl2 = '<?= $tgl_akhir ?>';
+        };
+        load_silent("inout_h/aluminium/diSet/" + tlg1 + "/" + tgl2, "#content");
+
+    }
+
+    function cetakExcel() {
+        var tgl_awal = $('#tgl_awal').val();
+        if (tgl_awal != '') {
+            var tlg1 = tgl_awal;
+        } else {
+            var tlg1 = '<?= $tgl_awal ?>';
+        };
+        var tgl_akhir = $('#tgl_akhir').val();
+        if (tgl_akhir != '') {
+            var tgl2 = tgl_akhir;
+        } else {
+            var tgl2 = '<?= $tgl_akhir ?>';
+        };
+
+        var url = "<?= site_url('inout_h/aluminium/diSetCetak/"+tlg1+"/"+tgl2+"') ?>";
+        window.open(url, "_blank");
+    }
+</script>
