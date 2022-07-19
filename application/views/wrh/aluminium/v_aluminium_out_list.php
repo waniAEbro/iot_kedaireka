254bffcbe15a50fe58eaab1f00dd22dc1d4ff26c
Jihan Ali Ahmad <jihanaliahmad@gmail.com>
2022-07-19 10:04:43 +0700

    jj

--- application/views/wrh/aluminium/v_aluminium_out_list.php
+++ application/views/wrh/aluminium/v_aluminium_out_list.php
@@ -3,7 +3,7 @@
     <div class="col-lg-12">
         <div class="box box-primary">
             <div class="box-header with-border">
-                <h3 class="box-title">Surat Jalan BOM Aluminium</h3>
+                <h3 class="box-title">Surat Jalan BOM Aluminium RSD</h3>
                 <div class="box-tools pull-right">
                     
                 </div>
