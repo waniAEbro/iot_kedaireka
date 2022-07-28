3df26d11b6be109fd23d00d1295cb63dc5c3f394
Jihan Ali Ahmad <jihanaliahmad@gmail.com>
2022-07-28 14:21:39 +0700

    cdc

--- application/controllers/wrh/aksesoris.php
+++ application/controllers/wrh/aksesoris.php
@@ -81,6 +81,7 @@ class aksesoris extends CI_Controller
         $data['s_total_out'] = $this->m_aksesoris->getTotalOutPerBulanCetak();
         $data['s_total_in_lalu']  = $this->m_aksesoris->getTotalInPerBulanCetakLalu();
         $data['s_total_out_lalu'] = $this->m_aksesoris->getTotalOutPerBulanCetakLalu();
+        $data['s_total_out_proses'] = $this->m_aksesoris->getTotalOutPerBulanCetakProses();
         $data['jenis_barang'] = "Aksesoris";
         $this->load->view('wrh/aksesoris/v_aksesoris_cetak_monitoring', $data);
     }
