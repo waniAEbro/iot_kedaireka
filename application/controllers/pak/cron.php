55b5cfe5f5a92ef3c50bf691048d0dcbd37a36b4
Jihan Ali Ahmad <jihanaliahmad@gmail.com>
2022-11-07 15:56:13 +0700

    dsd

--- application/controllers/pak/cron.php
+++ application/controllers/pak/cron.php
@@ -256,8 +256,10 @@ class Cron extends CI_Controller
             $this->db->where('id_item', $dd->id);
             $this->db->where('keranjang', $keranjang);
             $this->db->update('data_stock', $object);
-
             echo 'SUKSES update item : ' . $item_code . ' keranjang : ' . $keranjang_besar;
+            $this->db->where('id_item', $dd->id);
+            $this->db->where('keranjang', $keranjang);
+            $this->db->update('data_counter', $object);
         } else {
             echo 'GAGAL update item : ' . $item_code . ' keranjang : ' . $keranjang_besar;
         }
