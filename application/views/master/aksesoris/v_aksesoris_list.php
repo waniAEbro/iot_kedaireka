7bfc2575b2b5b17bbf8aaef2eaf57db9c830199d
rasikhkp <raxiest@gmail.com>
2023-09-01 13:47:07 +0700

    menambah tabel kedua di master aksesoris

--- application/views/master/aksesoris/v_aksesoris_list.php
+++ application/views/master/aksesoris/v_aksesoris_list.php
@@ -6,16 +6,57 @@
                 <h3 class="box-title">Master Aksesoris</h3>
                 <div class="box-tools pull-right">
                     <a class="btn btn-primary" onclick="cetakExcel()">Cetak</a>
-                    <?php
-                    $sesi = from_session('level');
-                    if ($sesi <= 2) {
-                        //echo button('load_silent("master/aksesoris/import/","#content")', 'Import Excel', 'btn btn-primary', 'data-toggle="tooltip" title="Import Excel"');
-                    }
-                    if ($sesi <= 3) {
-                        echo button('load_silent("master/aksesoris/form/base","#modal")', 'Add', 'btn btn-info', 'data-toggle="tooltip" title="Add"');
-                    }
-                    ?>
+                    
                 </div>
+            </div>
+            <div class="box-body">
+                <table width="100%" id="tableku2" class="table table-striped">
+                    <thead>
+                        <th width="5%">No</th>
+                        <th>Item code</th>
+                        <th>Deskripsi</th>
+                        <th>Satuan</th>
+                        <th>Supplier</th>
+                        <th>Lead Time</th>
+                        <th>Act</th>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $i = 1;
+                        foreach ($sudah->result() as $row) : ?>
+                            <tr id="output_data_<?= $row->id ?>" class="output_data">
+                                <td align="center"><?= $i++ ?></td>
+                                <td><?= $row->item_code ?></td>
+                                <td><?= $row->deskripsi ?></td>
+                                <td><?= $row->satuan ?></td>                                
+                                <td><?= $row->supplier ?></td>
+                                <td><?= $row->lead_time ?></td>
+                                <td align="center">
+                                    <?php
+                                    $sesi = from_session('level');
+                                    if ($sesi <= 3) {
+                                        echo button('load_silent("master/aksesoris/form/sub/' . $row->id . '","#modal")', 'Edit', 'btn btn-xs btn-info', 'data-toggle="tooltip" title="Edit"');
+                                    } else {
+                                        # code...
+                                    }
+                                    ?>
+                                    <a class="btn btn-xs btn-danger" href="javascript:void(0)" onClick="hapus(<?= $row->id ?>)">
+                                        hapus
+                                    </a>
+                                    <a target="_blank" href="<?= base_url('master/aksesoris/cetak_barcode/' . $row->id); ?>" class="btn btn-xs btn-primary">Cetak Barcode</a>
+                                </td>
+                            </tr>
+
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-12 mt-10">
+        <div class="box box-primary">
+            <div class="box-header with-border">
+                
             </div>
             <div class="box-body">
                 <table width="100%" id="tableku" class="table table-striped">
@@ -31,7 +72,7 @@
                     <tbody>
                         <?php
                         $i = 1;
-                        foreach ($aksesoris->result() as $row) : ?>
+                        foreach ($belum->result() as $row) : ?>
                             <tr id="output_data_<?= $row->id ?>" class="output_data">
                                 <td align="center"><?= $i++ ?></td>
                                 <td><?= $row->item_code ?></td>
@@ -65,6 +106,13 @@
 <script type="text/javascript">
     $(document).ready(function() {
         var table = $('#tableku').DataTable({
+            "ordering": true,
+            "scrollX": true,
+        });
+    });
+
+    $(document).ready(function() {
+        var table = $('#tableku2').DataTable({
             "ordering": true,
             "scrollX": true,
         });
